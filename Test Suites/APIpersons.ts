673f32d5559a8b10a6873d4c3bc0b59ea01dd878
irfan maulana <integer1507@gmail.com>
2026-06-08 23:50:42 +0700

    [update] test suites

---
+++ Test Suites/APIpersons.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>APIpersons</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>654e03d4-439a-4e7f-86fd-49cdbfdcb4cc</testSuiteGuid>
+   <testCaseLink>
+      <guid>acdbb166-5a87-4a48-96aa-92bc3fb0734a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Get Persons</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
